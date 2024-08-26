0283fd7ebdaacd9f74c41c97f5d6f940a934ef10
jwar28 <javierguerra2203@gmail.com>
2024-08-26 17:00:02 -0500

    pwa support added

--- next.config.js
+++ next.config.js
@@ -6,9 +6,6 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   reloadOnOnline: true,
   swcMinify: true,
   dest: 'public',
-  fallbacks: {
-    document: '/offline',
-  },
   workboxOptions: {
     disableDevLogs: true,
   },
